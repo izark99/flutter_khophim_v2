0d340ca8aaf283ce87f24b977b0493a9a642e204
izark <izarknguyen@gmail.com>
2021-01-24 19:24:19 +0700

    Add back button

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:khophim/helpers/constant.dart';
 import 'package:khophim/models/detail_model.dart';
 import 'package:khophim/services/res_service.dart';
@@ -43,81 +44,108 @@ class DetailPage extends StatelessWidget {
                 ),
               ),
               SafeArea(
-                child: SingleChildScrollView(
-                  child: Container(
-                    padding: PAD_ALL_10,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SIZED_BOX_H20,
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: CachedNetworkImage(
-                            imageUrl: "https://image.tmdb.org/t/p/w500/" +
-                                snapshot.data.posterPath,
-                            width: MediaQuery.of(context).size.width / 2,
-                            fit: BoxFit.cover,
-                            progressIndicatorBuilder:
-                                (context, url, downloadProgress) => Center(
-                              child: CircularProgressIndicator(
-                                  value: downloadProgress.progress),
+                child: Stack(
+                  children: [
+                    SingleChildScrollView(
+                      child: Container(
+                        padding: PAD_ALL_10,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SIZED_BOX_H20,
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: CachedNetworkImage(
+                                imageUrl: "https://image.tmdb.org/t/p/w500/" +
+                                    snapshot.data.posterPath,
+                                width: MediaQuery.of(context).size.width / 2,
+                                fit: BoxFit.cover,
+                                progressIndicatorBuilder:
+                                    (context, url, downloadProgress) => Center(
+                                  child: CircularProgressIndicator(
+                                      value: downloadProgress.progress),
+                                ),
+                                errorWidget: (context, url, error) => Center(
+                                  child: Icon(Icons.error, size: ICON_SIZE_M),
+                                ),
+                              ),
                             ),
-                            errorWidget: (context, url, error) => Center(
-                              child: Icon(Icons.error, size: ICON_SIZE_M),
+                            SIZED_BOX_H20,
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Container(
+                                padding: PAD_ALL_10,
+                                color: Theme.of(context)
+                                    .canvasColor
+                                    .withOpacity(0.8),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      snapshot.data.title,
+                                      style:
+                                          Theme.of(context).textTheme.headline4,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    SIZED_BOX_H05,
+                                    Text(
+                                      snapshot.data.voteAverage.toString() +
+                                          "/10",
+                                      style:
+                                          Theme.of(context).textTheme.headline5,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    SIZED_BOX_H15,
+                                    Text(
+                                      snapshot.data.overview.isEmpty
+                                          ? "CHƯA CÓ ĐÁNH GIÁ"
+                                          : snapshot.data.overview,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyText1
+                                          .copyWith(wordSpacing: 1.5),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
-                          ),
+                            SIZED_BOX_H15,
+                            CustomButton(
+                              onPressed: () async {
+                                String url =
+                                    "https://www.google.com/search?q=${snapshot.data.title.toString()}";
+                                await launch(url);
+                              },
+                              buttonName: "TÌM TRÊN GOOGLE",
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
                         ),
-                        SIZED_BOX_H20,
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Positioned(
+                      top: 5,
+                      left: 20,
+                      child: GestureDetector(
+                        child: ClipOval(
                           child: Container(
-                            padding: PAD_ALL_10,
+                            padding: PAD_ALL_05,
                             color:
                                 Theme.of(context).canvasColor.withOpacity(0.8),
-                            child: Column(
-                              children: [
-                                Text(
-                                  snapshot.data.title,
-                                  style: Theme.of(context).textTheme.headline4,
-                                  textAlign: TextAlign.center,
-                                ),
-                                SIZED_BOX_H05,
-                                Text(
-                                  snapshot.data.voteAverage.toString() + "/10",
-                                  style: Theme.of(context).textTheme.headline5,
-                                  textAlign: TextAlign.center,
-                                ),
-                                SIZED_BOX_H15,
-                                Text(
-                                  snapshot.data.overview.isEmpty
-                                      ? "CHƯA CÓ ĐÁNH GIÁ"
-                                      : snapshot.data.overview,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      .copyWith(wordSpacing: 1.5),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ],
+                            child: Icon(
+                              Icons.arrow_back_outlined,
+                              color: Theme.of(context).accentColor,
+                              size: ICON_SIZE_M,
                             ),
                           ),
                         ),
-                        SIZED_BOX_H15,
-                        CustomButton(
-                          onPressed: () async {
-                            String url =
-                                "https://www.google.com/search?q=${snapshot.data.title.toString()}";
-                            await launch(url);
-                          },
-                          buttonName: "TÌM TRÊN GOOGLE",
-                          textStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        )
-                      ],
+                        onTap: () => Get.back(),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ),
             ],
